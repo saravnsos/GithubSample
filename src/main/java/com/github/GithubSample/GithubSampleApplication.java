3bc72da1672bd7be5b46d34eabe22ffd5cccc698
saravnsos <saravnsos@gmail.com>
2025-09-22 12:11:57 +0530

    sample

---
+++ src/main/java/com/github/GithubSample/GithubSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.GithubSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSampleApplication.class, args);
+	}
+
+}
